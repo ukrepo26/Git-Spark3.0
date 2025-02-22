c3612ca5943276886af5c0928df15f9297753215
Umesh <umeshcse2026@gmail.com>
2025-02-22 16:19:39 +0530

    change in Spark file

--- Spark.java
+++ Spark.java
@@ -2,4 +2,8 @@ public class Spark {
     public static void main(String[] args) {
         System.out.println("   Hello");
     }
+
+    public void m1(){
+        System.out.println("m1");
+    }
 }
